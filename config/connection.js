46dc3a9b42c65567173b3eabd55a0c106a3f3625
JaredSchips <jared.schips@gmail.com>
2022-12-23 13:36:16 -0500

    Add mysql connection

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: 'root',
+      database: 'company_db'
+    },
+    console.log(`Connected to the company_db database.`)
+  );
+
+module.exports = db
