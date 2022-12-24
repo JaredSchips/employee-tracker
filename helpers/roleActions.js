478df797c0be75bf90051ead1dc1acee6a6e0c0f
JaredSchips <jared.schips@gmail.com>
2022-12-23 22:17:15 -0500

    Add roleActions.js

---
+++ helpers/roleActions.js
@@ -0,0 +1,43 @@
+inquirer = require('inquirer')
+const db = require('../config/connection')
+const departmentActions = require('./departmentActions')
+
+const view = async () => {
+    const [result, data] = await db.query('SELECT * FROM role')
+    return result
+}
+
+const add = async () => {
+    const departments = await departmentActions.view()
+    const departmentChoices = departments.map(row => {return {
+            name: row.name,
+            value: row.id
+        }
+    })
+    await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the role\'s title?'
+        },
+        {
+            type: 'number',
+            name: 'salary',
+            message: 'What is the role\'s salary?',
+            transformer: num => '$'+num
+        },
+        {
+            type: 'list',
+            name: 'departmentId',
+            message: 'What department does the role belong to?',
+            choices: departmentChoices
+        }
+    ]).then(async responses => {
+        const { title, salary, departmentId } = responses
+        await db.query('INSERT INTO role (title, salary, department_id) VALUES (?,?,?)',
+        [title, salary, departmentId])
+        console.log(`${title} added to database`)
+    })
+}
+
+module.exports = { view, add }
